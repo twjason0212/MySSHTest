78909d469b90e249d2e7a47c33bf72939dd0c11b
tcjason <tcjason@bajile.net>
2021-07-23 16:21:54 +0800

    11

--- compress-test.bat
+++ compress-test.bat
@@ -1,3 +1,17 @@
 "C:\Program Files\WinRAR\Rar.exe"  a deploy.zip -ep1 C:\Users\JASON\source\repos\WinFormsApp1\WinFormsApp1\bin\Debug\netcoreapp3.1\*
 
 echo rar part1
+
+setlocal enabledelayedexpansion
+echo please make sure you are admin
+set 7ZipDir=%ProgramFiles%\7-Zip
+
+set plegodemo=deploy.zip
+
+set curdir=%~dp0
+set deploydir=C:\api
+set "plegodemodir=!plegodemo:.zip=!"
+start /wait "" "!7ZipDir!\7z.exe" x !curdir!!plegodemo! -o!deploydir!\!plegodemodir!\ -aoa
+
+
+echo part2 end
