02f33b0f47170fe147447d92b40523a658026206
tcjason <tcjason@bajile.net>
2021-07-23 15:23:07 +0800

    11

--- compres-1.bat
+++ compres-1.bat
@@ -1,7 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 echo please make sure you are admin！
-echo 执行此脚本前，请确认已经安装解压缩程序7-Zip
 :::::::::: 配置参数 ::::::::::
 ::解压程序WinRAR.exe所在目录 32位%ProgramFiles(x86)% 或64位 %ProgramFiles%
 set 7ZipDir=%ProgramFiles%\7-Zip
