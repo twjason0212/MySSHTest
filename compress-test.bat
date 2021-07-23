625f9862ccebde3f054a5e8b7f630a0743255232
tcjason <tcjason@bajile.net>
2021-07-23 13:51:31 +0800

    update

--- compress-test.bat
+++ compress-test.bat
@@ -1,3 +1,7 @@
 "C:\Program Files\WinRAR\Rar.exe"  a deploy.rar -ep1 C:\Users\JASON\source\repos\WinFormsApp1\WinFormsApp1\bin\Debug\netcoreapp3.1\*
 
-"C:\Program Files\WinRAR\Rar.exe" x -y C:\ttest\deploy.rar C:\api\
+echo rar part1
+
+"C:\Program Files\WinRAR\Rar.exe" x -y C:\repos\MySSHTest\MySSHTest\deploy.rar C:\api\
+
+echo rar part2
