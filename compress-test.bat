b7796a2ec244f465d3f48016e9f3d15037f4a0d7
tcjason <tcjason@bajile.net>
2021-07-23 17:11:41 +0800

    1

--- compress-test.bat
+++ compress-test.bat
@@ -1,4 +1,6 @@
+"C:\Program Files\WinRAR\Rar.exe"  a deploy.zip -ep1 C:\Users\JASON\source\repos\WinFormsApp1\WinFormsApp1\bin\Debug\netcoreapp3.1\*
 
+echo rar part1
 
 setlocal enabledelayedexpansion
 echo please make sure you are admin
