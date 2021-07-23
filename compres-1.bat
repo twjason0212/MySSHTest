ae15ee6032eb49e280f1dd236e49fa84b785307c
tcjason <tcjason@bajile.net>
2021-07-23 15:25:51 +0800

    1

--- compres-1.bat
+++ compres-1.bat
@@ -1,17 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
-echo please make sure you are admin！
-:::::::::: 配置参数 ::::::::::
-::解压程序WinRAR.exe所在目录 32位%ProgramFiles(x86)% 或64位 %ProgramFiles%
+echo please make sure you are admin
 set 7ZipDir=%ProgramFiles%\7-Zip
 
-REM 当前目录下用来测试的压缩包
 set plegodemo=deploy.zip
 
-:: 解压
 set curdir=%~dp0
 set deploydir=C:\api
 set "plegodemodir=!plegodemo:.zip=!"
-:: C:\01_MyApp\7-Zip\7z.exe x newPack.zip -oc:\Doc -aoa
 start /wait "" "!7ZipDir!\7z.exe" x !curdir!!plegodemo! -o!deploydir!\!plegodemodir!\ -aoa
 
